8b1c5ae9e5962804b784c6c1c95dd0bea1d8a955
Sheyla Popovich <Sheylapopovich@gmail.com>
2021-09-12 14:49:31 -0600

    local host connection, success

--- server.js
+++ server.js
@@ -1,3 +1,62 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const PORT = process.env.PORT || 3001;
 
+const app = express();
+const chalk = require('chalk');
+
+
+let ownerOptions = ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Roles", "View All Departments", "Add Department", "Quit"];
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: "free",
+    database: 'employees_db'
+  }
+  );
+
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected as Id" + db.threadId);
+    startPrompt();
+});
+
+
+function startPrompt() {
+console.log(chalk.blue.bold(`===========================================================
+          🎇✨ Welcome to Staff Intelligence ✨🎇
+    ===================================================
+    Where you can view and manage your company employees 
+    ===================================================
+       to better organize your company successfully!
+    ===================================================`));
+
+
+    
+inquirer.prompt([{
+    name: "task",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ownerOptions,
+},
+])
+.then ((answer) => {
+    console.log(answer.task);
+});
+}
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
