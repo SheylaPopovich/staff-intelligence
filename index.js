cf73b068f3cafcf5bc1a4c4f74350138f23a7803
Sheyla Popovich <Sheylapopovich@gmail.com>
2021-09-25 08:42:30 -0600

    add employee function created

--- index.js
+++ index.js
@@ -61,8 +61,13 @@ inquirer.prompt([{
       break;
 
 
-      // case "Add Department":
-    // break;
+      case "Add Department":
+        addDepartment();
+    break;
+
+    case "Add Role":
+      addRole ();
+      break; 
 
     default: console.log("this is the default");//THIS IS THE ELSE IN IF STATEMENT:
       // code block
@@ -96,23 +101,106 @@ function viewAllEmployees () {
    });
 }
 
-//*****function to add to each table****
-// function addDepartment () {
-//   const newDepartment = () =>{
-//     inquirer.prompt([
-//       {
-//         type: 'input',
-//         name: 'newDepartment',
-//         message: 'What department would you like to add?',
-//       }
-//     ]);
-//   };
-// }
-// .then(  
-//   connection.query ("INSERT INTO department (department_name) VALUES (?);", [response.department_name]);
-//   if (err) throw err;
-//      console.table(response);
-//      startPrompt();
-// );
+// *****function to add to each table****
+function addDepartment () {
+
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'newDepartment',
+        message: 'What department would you like to add?',
+      }
+    ])
+    .then( (response) => {
+      connection.query ("INSERT INTO department (name) VALUES (?);", [response.newDepartment], (err, res) => {
+        if (err) throw err;
+        console.log(response);
+        startPrompt();
+      });
+    });
+
+}
 
  
+
+function addRole () {
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'newRole',
+      message: 'What role would you like to add?'
+    },
+    {
+      type: 'input',
+      name: 'newSalary',
+      message: 'What is the employee salary?'
+    },
+    {
+      type: 'input',
+      name: 'newDepartmentId',
+      message: 'What is the Department ID?'
+    }
+  ]).then((response) => {
+  connection.query('INSERT INTO role set ?',{
+    title: response.newRole,
+    salary: response.newSalary,
+    department_id: response.newDepartmentId,
+  },  (err, res) => {
+    if (err) throw err;
+    console.log(response);
+    startPrompt();
+  });
+  });
+}
+
+
+//ADD EMPLOYEE FUNCTION
+function addEmployee () {
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'newFirstName',
+      message: 'What is the employees first name?'
+    },
+    {
+      type:'input',
+      name: 'newLastName',
+      message: 'What is the employees last name?'
+    },
+    {
+      type: 'list',
+      name: 'newEmpRole',
+      message: 'What is the employees role?',
+      choices: 'Business Development Manager',
+      'Business Development Representative',
+      'Marketing Executive',
+      'Marketing Specialist',
+      'Sales Engineer',
+      'Chief Architect',
+      'Software Architect',
+      'Engineering Fellow',
+      'Customer Experience Specialist',
+      'Service Agent'
+    },
+    {
+      type: 'input',
+      name:'newEmpManager',
+      message: "Who is the employees manager?"
+    }
+  ]). then ((response) =>{
+    connection.query ('INSERT INTO employee set ?'. {
+      first_name: response.newFirstName,
+      last_name: response.newLastName,
+      role_id: response.newEmpRole,
+      manager_id: response.newEmpManager,
+    },  (err, res) => {
+      if (err) throw err;
+      console.log(response);
+      startPrompt();
+  });
+  });
+}
+
+//TRY TO UPDATE EMPLOYEE ROLE FUNCTION
